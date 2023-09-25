a6e4389fd86a2511e30f95733c77f5f3f5ef6960
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-09-25 17:46:48 -0400

    Initial commit

---
+++ src/main/java/br/com/code3r/exerciciosspringboot/ExerciciosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.code3r.exerciciosspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/code3r/exerciciosspringboot/ExerciciosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.code3r.exerciciosspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
